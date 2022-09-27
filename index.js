f5f7dc9e298ee056d0ccf7eeb1ab8a88c907c9d9
RubyBunny <rubybunny2501@gmail.com>
2022-09-27 21:21:03 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+var inputElement = document.getElementById("password");
+var placeholderElement = document.querySelector(".placeholder");
+var placeholderExample = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXX".split("");
+inputElement.addEventListener("input", function (e) {
+    var inputText = inputElement.value.split("");
+    var newPlaceholderExample = placeholderExample.slice(inputText.length, -1);
+    var newPlaceholder = "\u0020".repeat(inputText.length) + newPlaceholderExample.join("");
+    placeholderElement.innerText = newPlaceholder;
+    console.log(newPlaceholder);
+});
---
+++ index.ts
@@ -0,0 +1,12 @@
+const inputElement = document.getElementById("password") as HTMLInputElement;
+const placeholderElement = document.querySelector(".placeholder") as HTMLPreElement;
+const placeholderExample: string[] = "XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXXX".split("");
+
+inputElement.addEventListener("input", (e) => {
+    const inputText: string[] = inputElement.value.split("");
+    const newPlaceholderExample = placeholderExample.slice(inputText.length, -1);
+    const newPlaceholder: string = "\u0020".repeat(inputText.length) + newPlaceholderExample.join("");
+
+    placeholderElement.innerText = newPlaceholder;
+    console.log(newPlaceholder);
+});
